9f7205d4c37a6e245c96a91a197fbb4fbb069a30
Juanma El Mejor <juanma.correo2006@gmail.com>
2025-05-19 09:07:05 -0500

    reportes

--- apps/reports/src/main.ts
+++ apps/reports/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ReportsModule } from './reports.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReportsModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create(ReportsModule); 
+  await app.listen(3000); // Puerto donde escucha
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
